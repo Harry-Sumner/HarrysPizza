f7baf1572563f24c615185f5b2c9ff8fe1d501b3
Harry <harry.sumner@outlook.com>
2023-11-20 14:03:39 +0000

    - Fixed issues with images not loading on published website. - Fixed issue with viewport text and image adjustment. - Added privacy policy. - Added new pages; Menu, About, Help and Order - Added content to About page.

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HarrysPizza.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
